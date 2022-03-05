bcc0e0cd9df2b64fff06f0ef858fa18abb377d47
David Rickard <davricka@microsoft.com>
2022-03-05 15:40:54 -0800

    Fixed the string extraction.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
+import { i18n } from "@lingui/core";
+import { t } from "@lingui/macro";
+
+import { messages } from "../locale/pt-br/messages";
+
+i18n.load("pt-br", messages);
+i18n.activate("pt-br");
+
 const element = document.createElement("div");
 
-element.innerHTML = "hello world!";
+element.innerHTML = t`Hello world!`;
 
 document.body.append(element);
